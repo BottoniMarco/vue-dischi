c20cf711b2fff4cdea869555ad88509c60957bc9
marco bottoni <bottoni.mrc@gmail.com>
2020-12-17 17:10:59 +0100

    dischi 2.1

--- js/script.js
+++ js/script.js
@@ -11,20 +11,29 @@ var root = new Vue({
   el: "#root",
   data: {
     albums:[],
-    selected:"",
+    selected:"All",
+    genres:[],
   },
   methods: {
     albumGenre : function () {
+      this.albums.forEach((item, i) => {
+        if (!this.genres.includes(item.genre)) {
+          this.genres.push(item.genre);
+        }
+        console.log(this.genres);
+
+      });
+
       console.log(this.selected);
-      
     },
   },
   created: function (){
+    const self = this;
     axios.get('https://flynn.boolean.careers/exercises/api/array/music')
       .then(function (result) {
         let albumSpecs = result.data.response;
-        root.albums = albumSpecs;
-        console.log(root.albums);
+        self.albums = albumSpecs;
+        console.log(self.albums);
 
       });
   }
